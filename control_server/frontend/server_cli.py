d8659c6fbf93d63500d134dad82c7d01d9765cdf
AidenCMG <aiden.mckenzie@comcast.net>
2025-09-13 20:57:12 +0000

    Create batch task creation

--- control_server/frontend/server_cli.py
+++ control_server/frontend/server_cli.py
@@ -4,44 +4,97 @@ import shlex
 import argparse
 import time
 import sys
-
+from pathlib import Path
 
 SERVER_URL = "http://127.0.0.1:8000"
 
 
 def create_task(command_args: list):
     parser = argparse.ArgumentParser(prog="create_task", description="Creates a new task on the server")
-    parser.add_argument("--module", required=True, help="The name of the module for the task.")
+    parser.add_argument("-m", "--module", required=True, help="The name of the module for the task.")
 
-    formatted_payload = getPayload()
     args=parser.parse_args(command_args)
-
-    data = {
+    payload = get_payload()
+    
+    data_to_post = {
         "module": args.module,
-        "payload": formatted_payload
+        "payload": payload
     }
-    requests.post(f"{SERVER_URL}/admin/create-task", json=data)
+    requests.post(f"{SERVER_URL}/admin/create-task", json=data_to_post)
 
-def getPayload():
+def batch_create(command_args: list):
+    parser = argparse.ArgumentParser(prog="batch_create", description="Creates tasks in batches")
+    parser.add_argument("-m", "--module", required=True, help="The name of the module for the tasks.")
+    parser.add_argument("-c", "--chunks", required=True, help="Size of chunks to use")
+    parser.add_argument("-l", "--label", required=True,  help="Label specifiying what the file is")
+
+    args=parser.parse_args(command_args)
+ 
+    payload_data = get_module_fields()
+    chunker = make_chunker(int(args.chunks),args.label) #make this upload specified filepath instead of returning list
+    
+    for chunk in chunker:
+        payload_data[args.label] = chunk
+
+        data_to_post = {
+            "module": args.module,
+            "payload": payload_data
+        }
+        requests.post(f"{SERVER_URL}/admin/create-task", json=data_to_post) #Swap this for file download url
+    
+
+def get_payload():
+    label = input("Enter label: ")
+    path = input("Enter path to file: ")
+
+    common_data = get_module_fields()
+    common_data[label] = path
+    return (common_data)
+
+
+def get_module_fields():
     keepGoing = True
-    data_holder = []
-
+    data_holder = {}
     while(keepGoing):
-        field_name = input("Enter the name of the first field: ")
+        field_name = input("Enter the name of the field: ")
         field_value = input("Enter the fields value: ")
-        data_holder.extend([field_name,field_value])
+        data_holder[field_name] = field_value
 
         response = input("Enter another field?\n1) Yes\n2) No\n")
         if response == "2":
             keepGoing = False
-            return formatPayload(data_holder)
-            
+            return data_holder
+        
+def make_chunker(chunk_size: int, label: str):
+    path = Path(input("Enter path to file/files: "))
+    if path.is_dir():
+        directory_contents = list(path.iterdir())
 
-def formatPayload(payload: list):
-    formatted_payload = {}
-    for i in range(0,len(payload),2):
-        formatted_payload[payload[i]] = payload[i+1]
-    return formatted_payload
+        for i in range(0, len(directory_contents),chunk_size):
+            chunk = directory_contents[i:i+chunk_size]
+
+            str_paths = []
+            for p in chunk:
+                str_paths.append(str(p))
+            yield str_paths
+            
+    elif path.is_file():      #For text files
+        directory_name = Path("split_files")
+        directory_name.mkdir()
+            
+        file_contents = path.read_text()
+        lines = file_contents.splitlines()
+        for i in range(0,len(lines), chunk_size):
+            chunk = lines[i:i+chunk_size]
+            file_path = directory_name/str(i)
+            file_path.write_text("\n".join(chunk))
+            
+            yield str(file_path)
+        
+    else:
+        print("specified path does not exist")
+            
+ 
 
     
 def list_tasks(_):
@@ -83,7 +136,8 @@ command_mapper = {
     "create_task": create_task,
     "list_tasks": list_tasks,
     "list_clients": list_clients,
-    "dance_party": dance_party
+    "dance_party": dance_party,
+    "batch_create": batch_create
 }
 
 def run():
@@ -100,5 +154,5 @@ def run():
             command = args[0]
             command_args = args[1:]
             command_mapper[command](command_args)
-print(formatPayload(["key1","name","key2","food"]))
+
 run()
