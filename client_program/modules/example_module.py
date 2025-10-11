7909a9409a8bd3bbd3e6b4a3f25c40d4799229f2
AidenCMG <aiden.mckenzie@comcast.net>
2025-10-11 15:49:59 +0000

    Functional example module

--- client_program/modules/example_module.py
+++ client_program/modules/example_module.py
@@ -2,18 +2,22 @@ import sys
 import json
 
 def example_function(task: dict):
-    counter = 0
-    backwards_name = ""
-    for letter in task["name"]:
-        letter = task["name"][-1 - counter]
-        counter+=1
-        backwards_name = backwards_name + letter
-    
-    result = {
-        "name": task["name"],
-        "backwards name": backwards_name
-    }
-    print(json.dumps(result))
+    names = task["files/contents"]
+    results_list = []
+    for name in names:
+        counter = 0
+        backwards_name = ""
+        for letter in name:
+            letter = name[-1 - counter]
+            counter+=1
+            backwards_name = backwards_name + letter
+
+        result = {
+            "name": name,
+            "backwards name": backwards_name
+        }
+        results_list.append(result)
+    print(json.dumps(results_list))
 
 task = json.loads(sys.argv[1])
 example_function(task)
